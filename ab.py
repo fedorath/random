c4307b40e036e769cdb4a9ea74f1d31646e0aeaf
fedorath <kurtax.h1@googlemail.com>
2017-04-23 14:13:59 +0100

    Update ab.py

--- ab.py
+++ ab.py
@@ -22,7 +22,7 @@ while True:
 while not (pi2go.irLeft() or pi2go.irRight()):
     if pi2go.irCentre():
       pi2go.spinRight(speed)
-        time.sleep(1)
+      time.sleep(1)
       else:
         pi2go.forward(speed)
     pi2go.stop()
