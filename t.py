0286cdd2ae2e81702fa571b28795fdcb80f5bb44
fedorath <kurtax.h1@googlemail.com>
2017-04-23 22:48:36 +0100

    Update t.py

--- t.py
+++ t.py
@@ -4,34 +4,36 @@ from Adafruit_PWM_Servo_Driver import PWM
 from sgh_PCF8591P import sgh_PCF8591P
 
 GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN) #Right IR sensor module
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Activation button
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left IR sensor module
 
-GPIO.setup(26,GPIO.OUT) #Left motor control
-GPIO.setup(24,GPIO.OUT) #Left motor control
-GPIO.setup(19,GPIO.OUT) #Right motor control
-GPIO.setup(21,GPIO.OUT) #Right motor control
+GPIO.setup(11, GPIO.IN) #Right IR set to 11
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Front activates
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left IR set to 7
 
- #use pwm on inputs so motors don't go too fast
+GPIO.setup(26,GPIO.OUT) #Left forward motor
+GPIO.setup(24,GPIO.OUT) #Left reverse motor
+GPIO.setup(19,GPIO.OUT) #Right forward motor
+GPIO.setup(21,GPIO.OUT) #Right reverse motor
+
 GPIO.setup(26, GPIO.OUT)
-p = GPIO.PWM(26, 20)
-p.start(0)
+LF = GPIO.PWM(26, 20)#sets amount of power
+LF.start(0)
 
 GPIO.setup(24, GPIO.OUT)
-q = GPIO.PWM(24, 20)
-q.start(0)
+LR = GPIO.PWM(24, 20)#sets amount of power
+LR.start(0)
 
 GPIO.setup(19, GPIO.OUT)
-a = GPIO.PWM(19, 20)
-a.start(0)
+RF = GPIO.PWM(19, 20)#sets amount of power
+RF.start(0)
 
 GPIO.setup(21, GPIO.OUT)
-b = GPIO.PWM(21, 20)
-b.start(0)
+RR = GPIO.PWM(21, 20)#sets amount of power
+RR.start(0)
 
-speed = 100
+speed = 100#set speed
+
 flag=0
 while True:
 	j=GPIO.input(13)
@@ -40,71 +42,74 @@ while True:
 		print "Robot Activated",j
 	
 	while flag==1:
-		i=GPIO.input(7) #Listening for output from right IR sensor
-		k=GPIO.input(11) #Listening for output from left IR sensor
-		if i==0: #Obstacle detected on right IR sensor
-			print "Obstacle detected on Right",i 
-			#Move in reverse direction
-    			p.ChangeDutyCycle(0)
-    			q.ChangeDutyCycle(speed)
-    			a.ChangeDutyCycle(0)
-    			b.ChangeDutyCycle(speed)
-    			q.ChangeFrequency(speed + 5)
-    			b.ChangeFrequency(speed + 5)
+		i=GPIO.input(7) 
+		k=GPIO.input(11)
+		if i==0: 
+			print "Right Side Detected!",i 
+			#Reverse
+    			LF.ChangeDutyCycle(0)
+    			LR.ChangeDutyCycle(speed)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LR.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
+			#Turns Left
+    			LF.ChangeDutyCycle(0)
+   			LR.ChangeDutyCycle(speed)
+   			RF.ChangeDutyCycle(speed)
+    			RR.ChangeDutyCycle(0)
+    			LR.ChangeFrequency(speed + 5)
+    			RF.ChangeFrequency(speed + 5)
 
-    			p.ChangeDutyCycle(0)
-   			q.ChangeDutyCycle(speed)
-   			a.ChangeDutyCycle(speed)
-    			b.ChangeDutyCycle(0)
-    			q.ChangeFrequency(speed + 5)
-    			a.ChangeFrequency(speed + 5)
 
-
-		if k==0: #Obstacle detected on left IR sensor
-			print "Obstacle detected on Left",k
-    			p.ChangeDutyCycle(0)
-    			q.ChangeDutyCycle(speed)
-    			a.ChangeDutyCycle(0)
-    			b.ChangeDutyCycle(speed)
-    			q.ChangeFrequency(speed + 5)
-    			b.ChangeFrequency(speed + 5)
-
-    			p.ChangeDutyCycle(speed)
-    			q.ChangeDutyCycle(0)
-    			a.ChangeDutyCycle(0)
-    			b.ChangeDutyCycle(speed)
-    			p.ChangeFrequency(speed + 5)
-    			b.ChangeFrequency(speed + 5)
+		if k==0: #Left IR 
+			print "Left Side Detected",k
+			#Reverse
+    			LF.ChangeDutyCycle(0)
+    			LR.ChangeDutyCycle(speed)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LR.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
+			#Turns Right
+    			LF.ChangeDutyCycle(speed)
+    			LR.ChangeDutyCycle(0)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LF.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
 		elif i==0 and k==0:
-			print "Obstacles on both sides"
-    			p.ChangeDutyCycle(0)
-    			q.ChangeDutyCycle(speed)
-    			a.ChangeDutyCycle(0)
-    			b.ChangeDutyCycle(speed)
-    			q.ChangeFrequency(speed + 5)
-    			b.ChangeFrequency(speed + 5)
-
-    			p.ChangeDutyCycle(speed)
-    			q.ChangeDutyCycle(0)
-    			a.ChangeDutyCycle(0)
-    			b.ChangeDutyCycle(speed)
-    			p.ChangeFrequency(speed + 5)
-    			b.ChangeFrequency(speed + 5)
+			print "Left and Right Detected!!"
+			#Reverse
+    			LF.ChangeDutyCycle(0)
+    			LR.ChangeDutyCycle(speed)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LR.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
+			#Turns Right
+    			LF.ChangeDutyCycle(speed)
+    			LR.ChangeDutyCycle(0)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(speed)
+    			LF.ChangeFrequency(speed + 5)
+    			RR.ChangeFrequency(speed + 5)
 			time(4)
-		elif i==1 and k==1:	#No obstacles, robot moves forward
-			print "No obstacles",i
-			#Robot moves forward
-    			p.ChangeDutyCycle(speed)
-    			q.ChangeDutyCycle(0)
-    			a.ChangeDutyCycle(speed)
-    			b.ChangeDutyCycle(0)
-    			p.ChangeFrequency(speed + 5)
-    			a.ChangeFrequency(speed + 5)
+		elif i==1 and k==1:
+			print "Clear Route",i
+			#Forward
+    			LF.ChangeDutyCycle(speed)
+    			LR.ChangeDutyCycle(0)
+    			RF.ChangeDutyCycle(speed)
+    			RR.ChangeDutyCycle(0)
+    			LF.ChangeFrequency(speed + 5)
+    			RF.ChangeFrequency(speed + 5)
 		j=GPIO.input(13)
-		if j==1: #De activate robot on pushin the button
+		if j==1: 
 			flag=0
-			print "Robot De-Activated",j
-    			p.ChangeDutyCycle(0)
-    			q.ChangeDutyCycle(0)
-    			a.ChangeDutyCycle(0)
-    			b.ChangeDutyCycle(0)
+			print "Terminated!",j
+			#Stops
+    			LF.ChangeDutyCycle(0)
+    			LR.ChangeDutyCycle(0)
+    			RF.ChangeDutyCycle(0)
+    			RR.ChangeDutyCycle(0)
